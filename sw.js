6d48e06dc0cc64514521b88f9299d10e16a4b42d
tiac-occ <tsunghsun813@gmail.com>
2025-12-20 02:36:45 +0800

    Update sw.js v8

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
-// 步驟 1: 版本號升級！ (從 v6 改為 v7 以強制更新)
-const CACHE_NAME = 'shift-platform-cache-v7'; 
+// 步驟 1: 版本號升級！ (從 v7 改為 v8 以強制更新)
+const CACHE_NAME = 'shift-platform-cache-v8'; 
 const urlsToCache = [
   './',
   './index.html',
