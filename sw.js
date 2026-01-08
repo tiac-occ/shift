37faec6e7f7adbe6a5609eb6f13a3e0bfe749c74
tiac-occ <tsunghsun813@gmail.com>
2026-01-08 14:38:51 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
-// 步驟 1: 版本號升級！ (從 v9 改為 v9.1 以強制更新)
-const CACHE_NAME = 'shift-platform-cache-v9.1'; 
+// 步驟 1: 版本號升級！ (從 v9 改為 v9.2 以強制更新)
+const CACHE_NAME = 'shift-platform-cache-v9.2'; 
 const urlsToCache = [
   './',
   './index.html',
